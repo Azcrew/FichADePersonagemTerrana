c099fba5aebf9d4f72ab8d0e46a1fe5d6fd31f3e
azcrew <azcrew@Terrana.ddns.net>
2018-01-31 03:37:55 -0200

    implementação das funções de verificação de pontos para vantegens, desvantagens e pericias

--- scripts/createPersonage.js
+++ scripts/createPersonage.js
@@ -1,6 +1,7 @@
 var pv = 0;
 var rpv = 0;
 var modpv = 0;
+var modPeri = 0;
 
 var pm = 0;
 var rpm = 0;
@@ -11,6 +12,7 @@ var def = 0;
 
 var nivel = 0;
 var pontos = 0;
+var pericia = 0;
 
 var armadura = 0;
 var focus = 0;
@@ -26,23 +28,21 @@ var luz = 0;
 var terra = 0;
 var trevas = 0;
 
-var modClas = 0;
+var modClas = {};
 var ptsVant = 0;
-var modVant = 0;
+var modVant = {};
 var ptsDesv = 0;
-var modDesv = 0;
+var modDesv = {};
 var ptsPeri = 0;
-var modPeri = 0;
 
 const file = "db.php";
 const modStats = 10;
 const euler = 0.2718 / 10;
 
 //requisita dados ao banco
-function dbRequest(url)
-{
+function dbRequest(url) {
     var req = new XMLHttpRequest();
-    
+
     req.open("GET", url, false);
     req.send();
 
@@ -50,24 +50,22 @@ function dbRequest(url)
 }
 
 //requisita as informações necessarias ao site e altera seus valores referentes
-function selectRace(id)
-{
+function selectRace(id) {
     var url = file + "?t=race&i=" + id + "&f=";
     modpv = dbRequest(url + "modPV");
     modpm = dbRequest(url + "modPM");
     alterDetails();
 }
 
-function selectClass(id)
-{
+function selectClass(id) {
     var url = file + "?t=class&i=" + id + "&f=";
+    modPeri = dbRequest(url + "modKnow");
     modClas = dbRequest(url + "modifier");
-    //alert("mod class = " + modClas);
+    alterPoints();
 }
 
 //requisita as informações necessarias ao site e altera seus valores referentes 
-function selectAdventure(id)
-{
+function selectAdventure(id) {
     var url = file + "?t=adventure&i=" + id + "&f=";
     nivel = dbRequest(url + "level");
     pontos = dbRequest(url + "points");
@@ -76,25 +74,8 @@ function selectAdventure(id)
     alterDetails();
 }
 
-function selectBenefit(id)
-{
-    var url = file + "?t=benefits%i=" + id + "$f=";
-    alert(id);
-}
-
-function selectInjury(id)
-{
-
-}
-
-function selectKnowledge(id)
-{
-
-}
-
 //função necessaria por definir e verificar os pontos usados em caracteristicas e focus
-function changeCar()
-{
+function changeCar() {
     //adquire os valores definidos pelo usuario para as caracteristicas e focus
     //se o valor for negativo define ele como 0
     armadura = document.getElementById("armadura").value;
@@ -139,8 +120,7 @@ function changeCar()
 }
 
 //função responsavel por definir os detalhes do personagem
-function alterDetails()
-{
+function alterDetails() {
     //define os pontos de vida e de magia, e suas regenerações
     pv = nivel * resistencia * armadura * modpv;
     rpv = Math.trunc(pv * euler);
@@ -148,8 +128,7 @@ function alterDetails()
     rpm = Math.trunc(pm * euler);
 
     //define 'atk' como o calculo de ataque baseado na caracteristica de combate mais alta
-    if(focus >= forca && focus>= precisao)
-    {
+    if (focus >= forca && focus >= precisao) {
         var elemento = 0;
 
         elemento = (agua > elemento) ? agua : elemento;
@@ -160,21 +139,20 @@ function alterDetails()
         elemento = (trevas > elemento) ? trevas : elemento;
 
         atk = (parseInt(elemento) + parseInt(focus)) * habilidade;
-        if(!atk)
-        {
+        if (!atk) {
             atk = 0;
         }
     }
-    else if (forca >= focus && forca >= precisao)
-    {
+    else if (forca >= focus && forca >= precisao) {
         atk = forca * habilidade;
     }
-    else
-    {
+    else {
         atk = precisao * habilidade;
     }
 
     def = armadura * resistencia;
+
+    pericia = habilidade * modPeri
 
     //exibe os detalhes
     document.getElementById("PV").innerText = pv;
@@ -185,13 +163,13 @@ function alterDetails()
     document.getElementById("defesa").innerText = def;
 }
 
-//função responsavel por verificar e exibir os pontos
-function alterPoints()
-{
+//responsavel por verificar e exibir os pontos de personagem
+function alterPoints() {
     //usado para nao alterar os valores originais
     var pontosTemp = pontos;
     var focusTemp = focus;
-    
+    var knowledgeTemp = pericia;
+
     //Caracteristicas
     pontosTemp -= armadura;
     pontosTemp -= focus;
@@ -200,10 +178,9 @@ function alterPoints()
     pontosTemp -= resistencia;
     pontosTemp -= precisao;
 
-    //vantagens, Desvantagens e Pericias
+    //vantagens e Desvantagens
     pontosTemp -= ptsVant;
-    pontosTemp -= ptsDesv;
-    pontosTemp -= ptsPeri;
+    pontosTemp -= ptsDesv; //negativo ou zero
 
     //Focus
     focusTemp -= agua;
@@ -213,17 +190,71 @@ function alterPoints()
     focusTemp -= terra;
     focusTemp -= trevas;
 
+    //pericias
+    knowledgeTemp -= ptsPeri;
+
     //limite de pontos atingidos
-    if(pontosTemp < 0)
-    {  
+    if (pontosTemp < 0) {
         alert("Limite de pontos ultrapassado em " + Math.abs(pontosTemp) + " ponto(s).");
         pontosTemp = 0;
     }
     //limite de focus atingido
-    else if (focusTemp < 0)
-    {
+    else if (focusTemp < 0) {
         alert("Limite de Focus ultrapassado em " + Math.abs(focusTemp) + " ponto(s).");
+    }
+    else if(knowledgeTemp < 0)
+    {
+        alert("Limite de Pontos de Perícias ultrapassado em " + Math.abs(knowledgeTemp) + " ponto(s)");
     }
     //exibe os pontos restantes
     document.getElementById("pontos").innerText = pontosTemp;
+}
+
+window.onload = function () {   //TODO Mudar tudo para este metodo, simplifica o html e da mais liberdade (Apenas na segunda versão)
+
+    var benefit = document.getElementById("benefit");
+    var injury = document.getElementById("injury");
+    var knowledge = document.getElementById("knowledge");
+
+    benefit.addEventListener("change", function () {
+        var idVant = [].slice.call(benefit.selectedOptions).map(a => a.value);
+        var temp = 0;
+
+        for (var i = 0; i < idVant.length; i++) {
+            var url = file + "?t=benefit&i=" + idVant[i] + "&f=";
+
+            temp += parseInt(dbRequest(url + "cost"));
+            modVant[i] = dbRequest(url + "modifier");
+        }
+        ptsVant = temp;
+        alterPoints();
+    });
+
+    injury.addEventListener("change", function () {
+        var idDesv = [].slice.call(injury.selectedOptions).map(a => a.value);
+        var temp = 0;
+
+        for (var i = 0; i < idDesv.length; i++) {
+            var url = file + "?t=injury&i=" + idDesv[i] + "&f=";
+
+            temp -= parseInt(dbRequest(url + "cost"));
+            modDesv[i] = dbRequest(url + "modifier");
+        }
+        ptsDesv = temp;
+        alterPoints();
+    });
+
+    knowledge.addEventListener("change", function () {
+        var idPeri = [].slice.call(knowledge.selectedOptions).map(a => a.value);
+        var temp = 0;
+
+        for (var i = 0; i < idPeri.length; i++) {
+            var url = file + "?t=knowledge&i=" + idPeri[i] + "&f=";
+
+            temp += parseInt(dbRequest(url + "cost"));
+        }
+        ptsPeri = temp;
+        alterPoints();
+    });
+
 }
