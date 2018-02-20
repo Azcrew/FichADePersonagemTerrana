9e1fd81bd18fa2dd57754c6d7dd7fbc131cb1e93
azcrew <bspol.323@gmail.com>
2018-02-19 22:31:09 -0300

    not

--- lib/database.php
+++ lib/database.php
@@ -1,5 +1,5 @@
 <?php
-	//Exex command
+	//Execute command
 	function DBExecute($query)
 	{
 		$link = DBConnect();
@@ -19,9 +19,9 @@
 		$fields = implode(", ", array_keys($data));
 		$values = "'".implode("', '", $data)."'"; 
 		
-        $query = "INSERT INTO {$table} ( {$fields} ) VALUES ( $values )";
-        
-        return DBExecute($query);
+		$query = "INSERT INTO {$table} ( {$fields} ) VALUES ( $values )";
+
+		return DBExecute($query);
 	}
 	
 	//Read database register
@@ -61,9 +61,8 @@
 		$where = ($where) ? " WHERE {$where}" : null;
 		
 		$query = "UPDATE {$table} SET {$fields}{$where}";
-		$query = DBExecute($query);
 		
-		return $query;
+		return DBExecute($query);
 	}
 	
 	function DBDelete($table, $where)
@@ -73,7 +72,31 @@
 		$where = ($where) ? " WHERE {$where}" : null;
 		
 		$query = "DELETE FROM {$table}{$where}";
-		DBExecute($query);
+		return DBExecute($query);
 	}
 	
+	function DBSearch($table, $where)
+	{
+		$table = DB_PREFIX.'_'.$table;
+
+		$where = ($where) ? " WHERE {$where}" : null;
+
+		$query = "SELECT * FROM {$table}{$params}";
+        
+		$result = DBExecute($query);
+		
+		if(!mysqli_num_rows($result))
+		{
+			return false;
+		}
+		else
+		{
+			while($res = mysqli_fetch_assoc($result))
+			{
+				$data[] = $res; 
+			}
+			return $data;
+		}
+
+	}
 ?>
