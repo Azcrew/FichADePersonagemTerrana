e7fbab257f669790ae393d3a3646949438e6c243
azcrew <bspol.323@gmail.com>
2018-02-22 18:08:17 -0300

    Cange on encript

--- class/character.class.php
+++ class/character.class.php
@@ -1,109 +1,109 @@
 <?php
-   /** 
-    * Character Sheet
-    * 
-    * PHP version 7.2
-    * 
-    * @category  Games
-    * @package   Terrana
-    * @author    Bruno José Spolavori <bspol.323@gmail.com>
-    * @copyright 2018 Bruno José Spolavori
-    * @license   terrana.ddns.net/license Copyrigth
-    * @link      terrana.ddns.net
-    */
-    
+/** 
+* Character Sheet
+* 
+* PHP version 7.2
+* 
+* @category  Games
+* @package   Terrana
+* @author    Bruno José Spolavori <bspol.323@gmail.com>
+* @copyright 2018 Bruno José Spolavori
+* @license   terrana.ddns.net/license Copyrigth
+* @link      terrana.ddns.net
+*/
+
 class Character
 {
     //##TODO Transfer all set code to a just one medoth with data loop
     protected $character = array();
-    
+
     /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    * Construction Function
+    * 
+    * @param Character $character if have a formated array
+    * 
+    * @return null
+    */
     function _construct($character)
     {
         $this->character = $character;
     }
     
     /** 
-     * Construction Function
-     * 
-     * @param player       $player       int
-     * @param OnlyPlayable $onlyPlayable bool
-     * 
-     * @return null
-     */
+    * Construction Function
+    * 
+    * @param player       $player       int
+    * @param OnlyPlayable $onlyPlayable bool
+    * 
+    * @return null
+    */
     function setPlayer($player, $onlyPlayable)
     {
         $this->character['player'] = $player;
         
         $this->character['onlyplayable'] = $onlyPlayable;
     }
-
+    
     /** 
-     * Construction Function
-     * 
-     * @param Name $name Character name
-     * 
-     * @return null
-     */
+    * Construction Function
+    * 
+    * @param Name $name Character name
+    * 
+    * @return null
+    */
     function setName($name)
     {
         $this->character['name'] = $name; 
     }
     
     /** 
-     * Construction Function
-     * 
-     * @param race $race Character race
-     * 
-     * @return null
-     */
+    * Construction Function
+    * 
+    * @param race $race Character race
+    * 
+    * @return null
+    */
     function setRace($race)
     {
         $this->character['race'] = $race;
     }
     
     /**
-     * Construction Function
-     * 
-     * @param class $class Character class
-     * 
-     * @return null
-     */
+    * Construction Function
+    * 
+    * @param class $class Character class
+    * 
+    * @return null
+    */
     function setClass($class)
     {
         $this->character['class'] = $class;
     }
     
     /********************************************************************
-     * Construction Function
-     * 
-     * @param adventure $adventure belongs to adventure
-     * 
-     * @return null
-     */
+    * Construction Function
+    * 
+    * @param adventure $adventure belongs to adventure
+    * 
+    * @return null
+    */
     function setAdventure($adventure)
     {
         $this->character['adventure'] = $adventure;
     }
     
     /** 
-     * Construction Function
-     * 
-     * @param armor      $armor      int
-     * @param focus      $focus      int
-     * @param strength   $strength   int
-     * @param ability    $ability    int
-     * @param resistence $resistence int
-     * @param accuracy   $accuracy   int
-     * 
-     * @return null
-     */
+    * Construction Function
+    * 
+    * @param armor      $armor      int
+    * @param focus      $focus      int
+    * @param strength   $strength   int
+    * @param ability    $ability    int
+    * @param resistence $resistence int
+    * @param accuracy   $accuracy   int
+    * 
+    * @return null
+    */
     function setAspects($armor, $focus, $strength, $ability, $resistence, $accuracy)
     {
         $aspects = array(
@@ -118,17 +118,17 @@ class Character
     }
     
     /** 
-     * Construction Function
-     * 
-     * @param water    $water    int
-     * @param air      $air      int
-     * @param fire     $fire     int
-     * @param ligth    $ligth    int
-     * @param earth    $earth    int
-     * @param darkness $darkness int
-     * 
-     * @return null
-     */
+    * Construction Function
+    * 
+    * @param water    $water    int
+    * @param air      $air      int
+    * @param fire     $fire     int
+    * @param ligth    $ligth    int
+    * @param earth    $earth    int
+    * @param darkness $darkness int
+    * 
+    * @return null
+    */
     function setFocus($water, $air, $fire, $ligth, $earth, $darkness)
     {
         $focus = array(
@@ -142,119 +142,47 @@ class Character
         $this->character['focus'] = json_encode($focus, JSON_FORCE_OBJECT | JSON_NUMERIC_CHECK);
     }
     
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function setBenefit($benefit)
     {
         $this->character['benefits'] = json_encode($benefit, JSON_FORCE_OBJECT | JSON_NUMERIC_CHECK);
     }
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function addBenefit(){ }
-
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
+    
     function removeBenefit(){ }
     
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function setInjury($injury)
     {   
         $this->character['injurys'] = json_encode($injury, JSON_FORCE_OBJECT | JSON_NUMERIC_CHECK);
     }
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function addInjury(){ }
-
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
+    
     function removeInjury(){ }
     
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function setKnowledge($knowledge)
     {
         $this->character['knowledges'] = json_encode($knowledge, JSON_FORCE_OBJECT | JSON_NUMERIC_CHECK);
     }
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function addKnowledge(){ }
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function levelUpKnowledge(){ }
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function setHistory($history)
     {
         $this->character['history'] = $history;
     }
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function addHistory($data)
     {   
     }
-    /** 
-     * Construction Function
-     * 
-     * @param Character $character if have a formated array
-     * 
-     * @return null
-     */
+    
     function setLevel($level)
     {
         $this->character['level'] = $level; 
@@ -293,32 +221,31 @@ class Character
     
     function decrypt()
     {
-        $data = base64_decode($cryptedCharacter);
+        $data = base64_decode($this->cryptedCharacter);
         $dataToDecrypt = mb_substr($data, SODIUM_CRYPTO_AEAD_CHACHA20POLY1305_IETF_NPUBBYTES, null, '8bit');
         $IV = mb_substr($data, 0, SODIUM_CRYPTO_AEAD_CHACHA20POLY1305_IETF_NPUBBYTES, '8bit');
         
-        $dataNoCrypt = '';
-        //$key =  sodium_crypto_aead_chacha20poly1305_ietf_keygen();
+        $dataNoCrypt = $this->character['password']; 
         
-        $TextoClaro = sodium_crypto_aead_chacha20poly1305_ietf_decrypt($dataToDecrypt, $dataNoCrypt, $IV, $key);
-        echo "entrou";
-        return $TextoClaro;
+        $data = sodium_crypto_aead_chacha20poly1305_ietf_decrypt($dataToDecrypt, $dataNoCrypt, $IV, $this->key);
+        return $data;
     }
+
     function encrypt()
-    {
-        global $cryptedCharacter;
-        global $key;
+    {   
         $dataToCrypt = json_encode($this->character);
-        $dataNoCrypt = 'this is a rpg character file on terrana system'; 
+        $dataNoCrypt = $this->character['password']; 
         
         $IV = random_bytes(SODIUM_CRYPTO_AEAD_CHACHA20POLY1305_IETF_NPUBBYTES);
         $key =  sodium_crypto_aead_chacha20poly1305_ietf_keygen();
         
         $dataCrypted = sodium_crypto_aead_chacha20poly1305_ietf_encrypt($dataToCrypt, $dataNoCrypt, $IV, $key);
         
-        $dataCrypted = base64_encode($IV . $dataCrypted);
-        return $cryptedCharacter = $dataCrypted;
+        $this->key = $key;
+        $this->cryptedCharacter = base64_encode($IV . $dataCrypted);
+        return $this->cryptedCharacter;
     }
+
     function getCharacter()
     {  
         return $this->character;
