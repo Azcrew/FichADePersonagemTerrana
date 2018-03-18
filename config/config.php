f12bbc54fc882753ab8489df3bcd64e38c48b904
azcrew <bspol.323@gmail.com>
2018-03-18 02:21:32 -0300

    adicionando arquivos padrões de configurações

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+    /*
+     *Database Settings
+     */
+    define('DB_HOSTNAME', '');
+    define('DB_USERNAME', '');
+    define('DB_DATABASE', '');
+    define('DB_PREFIX', '');
+    define('DB_CHARSET', '');
+
+    /*
+     *HTML Settings
+     */
+    define('HTML_CHARSET', 'utf-8');
+    define('HTML_TITLE', 'Terrana');
+    define('MAIN_CSS', '../styles/main.css');
+
+    /*
+     *  Encryption Settings
+     */
+    define('HASH_CRIP', '');
+    define('REVERSE_CRIP', '');
