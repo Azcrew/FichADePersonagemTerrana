5ed50571934f0c00827f341a53ff418316111506
BuildTools <unconfigured@null.spigotmc.org>
2018-05-10 19:05:18 -0300

    Holy commit

--- scripts/sheet.js
+++ scripts/sheet.js
@@ -6,6 +6,8 @@ var focusOK = false;
 
 const MOD_REGEM = 0.1;
 const HOST = '192.168.15.10:8000';
+//const HOST = 'terrana.ddns.net:8000';
+
 
 var addSelectOptions = function(table){
     $.get( `https://${HOST}/${table}/`, function(data) {
