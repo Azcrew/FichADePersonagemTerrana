a92ab48ab809e0931dfdf24925f4e5b26bfb74fe
BuildTools <unconfigured@null.spigotmc.org>
2018-05-10 19:19:00 -0300

    Holy Commit

--- scripts/viewCharacter.js
+++ scripts/viewCharacter.js
@@ -1,15 +1,36 @@
-const HOST = 'localhost:8000';
-var site = window.location.href;
+const HOST = 'holy:8000';
+var character = [];
 
-$().ready(function(){
-    var character = prompt("ID do Personagem");
-    character = character ? character : 0;
+var fieldName = function(field, id){
+    $.get( `https://${HOST}/${field}/${id}`, function(data) {
+    $(`#${field}`).text(data[0]['name']);
+});
+}
 
-    url = `https://${HOST}/character/${character}`;
+var getCharacter = function(){
+    var char;// = prompt("ID do Personagem");
+    char = char ? char : 1;
+    url = `https://${HOST}/character/${char}`;
     $.get(url, function(data){
+<<<<<<< HEAD
         data = data[0];
         print(data);
 
     });
     
 })
+=======
+        obj = JSON.parse(data[0]['json']);
+        for(var prop in obj){
+            character.push(obj[prop]);
+        }
+        
+    });
+}
+
+$().ready(function(){
+    getCharacter();
+    console.log(character);
+    fieldName('race', 1);
+})
+>>>>>>> 3671e0dd9f0e34e441bab767eb014dede7b891bb
